98f279e5d91c849e4a90e387463cfca1c865427e
Martin Kukuruzar <martinkukuruzar@gmail.com>
2023-04-15 10:57:08 +0200

    Connect do Mongo

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
-import {Landing, Dashboard, Error, Register} from "./pages";
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import { Landing, Dashboard, Error, Register } from "./pages";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter >
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/landing" element={<Landing />} />
-      <Route path="*" element={<Error />} />
-      
-
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
     </BrowserRouter>
   );
 }
