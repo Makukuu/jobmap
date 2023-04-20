7bb4c6082fb736fdd9697006974e26faf85fbfe0
Martin Kukuruzar <martinkukuruzar@gmail.com>
2023-04-20 14:02:46 +0200

    Setup for deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,6 +41,7 @@ const login = async (req, res) => {
   }
   const token = user.createJWT();
   user.password = undefined;
+
   res.status(StatusCodes.OK).json({ user, token, location: user.location });
   res.send("login user");
 };
